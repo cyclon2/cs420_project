f3bb3e94a4dbdd330aaae8ab76f0f894a35476a1
cyclon2 <cyclon2@kaist.ac.kr>
2016-10-09 00:39:18 +0900

    DFS search

--- com.py
+++ com.py
@@ -7,55 +7,119 @@ tokens = (
 	)
 
 
-## bi operation ##
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_EQUALS = r'='
-t_LT = r'<'
-t_GT = r'>'
-t_LE = r'<='
-t_GE = r'>='
-t_EQ = r'=='
-t_NE = r'!='
-t_COMMA = r'\,'
-t_SEMICOLON = r';'
-t_COLON = r':'
-
-##	closure		##
-
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRK = r'\['
-t_RBRK = r'\]'
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
-
-##		num		##
-
-t_INTNUM = r'[0-9]+'
-t_FLOATNUM = r'[0-9]+.[0-9]+'
-
-##	keyword		##
-
-t_INT = r'int'
-t_FLOAT = r'float'
-t_RETURN = r'return'
-t_WHILE = r'while'
-t_DO = r'do'
-t_FOR = r'for'
-t_IF = r'if'
-t_ELSE = r'else'
-t_SWITCH = r'switch'
-t_CASE = r'case'
-t_DEFAULT = r'default'
-t_BREAK = r'break'
-
-
 
 
 t_ignore = " \t"
+
+def t_INTNUM(t):
+	r'[0-9]+'
+	print(t.value, end="")
+	return t
+
+def t_FLOATNUM(t):
+	r'[0-9]+.[0-9]+'
+	print(t.value, end="")
+	return t
+
+def t_PLUS(t):
+	r'\+'
+	print(t.value, end=" ")
+	return t
+
+def t_MINUS(t):
+	r'-'
+	print(t.value, end=" ")
+	return t
+
+def t_TIMES(t):
+	r'\*'
+	print(t.value, end=" ")
+	return t
+
+def t_DIVIDE(t):
+	r'/'
+	print(t.value, end=" ")
+	return t
+
+def t_EQUALS(t):
+	r'='
+	print(t.value, end=" ")
+	return t
+
+def t_LT(t):
+	r'<'
+	print(t.value, end=" ")
+	return t
+
+def t_GT(t):
+	r'>'
+	print(t.value, end=" ")
+	return t
+
+def t_LE(t):
+	r'<='
+	print(t.value, end=" ")
+	return t
+
+def t_GE(t):
+	r'>='
+	print(t.value, end=" ")
+	return t
+
+def t_EQ(t):
+	r'=='
+	print(t.value, end=" ")
+	return t
+
+def t_NQ(t):
+	r'!='
+	print(t.value, end=" ")
+	return t
+
+def t_COMMA(t):
+	r','
+	print(t.value, end =" ")
+	return t
+
+def t_SEMICOLON(t):
+	r';'
+	print(t.value, end=" ")
+	return t
+
+def t_COLON(t):
+	r':'
+	print(t.value, end=" ")
+	return t
+
+def t_LPAREN(t):
+	r'\('
+	print(t.value, end="")
+	return t
+
+def t_RPAREN(t):
+	r'\)'
+	print(t.value, end="")
+	return t
+
+def t_LBRK(t):
+	r'\['
+	print(t.value, end="")
+	return t
+
+def t_RBRK(t):
+	r'\]'
+	print(t.value, end="")
+	return t
+
+def t_LBRACE(t):
+	r'\{'
+	print(t.value, end="")
+	return t
+
+def t_RBRACE(t):
+	r'\}'
+	print(t.value, end="")
+	return t
 
 def t_ID(t):
 	r'[A-Za-z][A-Za-z0-9_]*'
@@ -94,11 +158,12 @@ def t_ID(t):
 	
 	elif t.value == 'break':
 		t.type = 'BREAK'	
-
+	print(t.value, end= " ")
 	return t
 
 def t_newline(t):
 	r'\n+'
+	print()
 	t.lexer.lineno += t.value.count("\n")
 
 def t_error(t):
@@ -119,63 +184,52 @@ precedence = (
 	('right','UMINUS'),
 )
 # dictionary of names
-
-
 def p_Program(t):
 	'''Program : DeclList FuncList
 			| DeclList
 			| FuncList 
 			|  '''
-	print("program" , t.slice)
 
 def p_DeclList(t):
 	'''DeclList : Declaration 
 			| DeclList Declaration'''
-	print("DeclList ", t.slice)
 
 def p_FuncList(t):
 	'''FuncList : Function
 			| FuncList Function'''
-	print("FucnList ", t.slice)
 
 def p_Declaration(t):
 	'Declaration : Type IdentList SEMICOLON'
-	print("Declaration : ", t.slice)
 
 def p_IdentList(t):
 	'''IdentList : identifier 
 			| IdentList COMMA identifier'''
-	print("IdentList : ", t.slice)
 
 def p_identifier(t):
 	'''identifier : ID 
 				| ID LBRK INTNUM RBRK '''
-	print("identifier : ", t.slice)
 	
 
 def p_Function(t):
 	'''Function : Type ID LPAREN RPAREN CompoundStmt
 			| Type ID LPAREN ParamList RPAREN CompoundStmt'''
-	print("Function :", t.slice)
 
 def p_ParamList(t):
 	'''ParamList : Type identifier
 			| ParamList COMMA Type identifier'''
-	print("paramList", t.slice)
 
 def p_Type(t):
 	'''Type : INT
 			| FLOAT'''
-	print("type : ", t.stack, t.slice)
 
 def p_CompoundStmt(t):
 	'''CompoundStmt : LBRACE DeclList StmtList RBRACE
 			| LBRACE StmtList RBRACE'''
-	print("CompoundStmt : ", t.slice)
 
 def p_StmtList(t):
 	'''StmtList : Stmt StmtList
 			| '''
+
 def p_Stmt(t):
 	'''Stmt : AssignStmt
 			| CallStmt
@@ -186,48 +240,38 @@ def p_Stmt(t):
 			| SwitchStmt
 			| CompoundStmt 
 			| SEMICOLON'''
-	print("Stmt ", t.slice)
 
 def p_AssignStmt(t):
 	'AssignStmt : Assign SEMICOLON'
-	print("AssignStmt : ", t.slice)
 
 def p_Assign(t):
 	'''Assign : ID EQUALS Expr
 			| ID LBRK Expr RBRK EQUALS Expr'''
-	print("Assign : ", t.slice)
 
 def p_CallStmt(t):
 	'CallStmt : Call SEMICOLON'
-	print("CallStmt : ",t.slice)
 
 def p_Call(t):
 	'''Call : ID LPAREN ArgList RPAREN
 			| ID LPAREN RPAREN'''
-	print("CAll : ", t.slice)
 
 def p_RetStmt(t):
 	'''RetStmt : RETURN SEMICOLON
 			| RETURN Expr SEMICOLON'''
-	print("RetStmt ", t.slice)
 
 def p_WhileStmt(t):
 	'''WhileStmt : WHILE LPAREN Expr RPAREN Stmt
 			| DO Stmt WHILE LPAREN Expr RPAREN'''
-	print("Whilestmt ", t.slice)
 
 def p_ForStmt(t):
 	'ForStmt : FOR LPAREN Assign SEMICOLON Expr SEMICOLON Assign RPAREN Stmt'
-	print("for ",  t.slice)
 
 def p_IfStmt(t):
 	'''IfStmt : IF LPAREN Expr RPAREN Stmt
 			| IF LPAREN Expr RPAREN Stmt ELSE Stmt'''
-	print("IF else : ", t.slice)
 
 def p_SwitchStmt(t):
 	'''SwitchStmt : SWITCH LPAREN identifier RPAREN LBRACE CaseList RBRACE'''
-	print("switch : ", t.slice)
 
 def p_CaseList(t):
 	'''CaseList : CaseList CASE INTNUM COLON StmtList BREAK SEMICOLON DEFAULT COLON StmtList BREAK SEMICOLON
@@ -242,7 +286,6 @@ def p_CaseList(t):
 			| CASE INTNUM COLON StmtList DEFAULT COLON StmtList
 			| CASE INTNUM COLON StmtList BREAK SEMICOLON
 			| CASE INTNUM COLON StmtList'''
-	print("case : ", t.slice)
 
 def p_Expr(t):
 	'''Expr : Unop
@@ -253,11 +296,9 @@ def p_Expr(t):
 			| ID 
 			| ID LBRK Expr RBRK 
 			| LPAREN Expr RPAREN'''
-	print("expr : ", t.slice)
 
 def p_unop(t):
 	'Unop : MINUS Expr %prec UMINUS'
-	print("unop : ",t.slice)
 
 def p_Bioperation(t):
 	'''Bioperation : Expr PLUS Expr
@@ -270,11 +311,10 @@ def p_Bioperation(t):
 			| Expr LE Expr
 			| Expr EQ Expr
 			| Expr NE Expr'''
-	print("bioper : ", t.slice)
+
 def p_ArgList(t):
 	'''ArgList : Expr
 			| ArgList COMMA Expr'''
-	print(t.slice)
 
 def p_error(t):
 	print("Syntax error at '%s'" % t.value)
@@ -283,11 +323,15 @@ def p_error(t):
 
 import ply.yacc as yacc
 parser = yacc.yacc()
-
+'''
 while True:
 	try:
 		s = input('>> ')
 	except EOFError:
 		break
 	parser.parse(s)
+'''
 
+f = open("test.txt")
+l = f.read()
+parser.parse(l)
